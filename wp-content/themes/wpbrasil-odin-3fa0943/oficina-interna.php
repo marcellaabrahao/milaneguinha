f09f2b95a12d31d6f424b38c467325769b5300b6
Marcella Garcia Abrahão <marcella.abrahao@gmail.com>
2017-05-11 00:05:22 -0300

    Galeria oficinas

--- wp-content/themes/wpbrasil-odin-3fa0943/oficina-interna.php
+++ wp-content/themes/wpbrasil-odin-3fa0943/oficina-interna.php
@@ -21,7 +21,38 @@
 				
 			<div class="container">
 				<div class="row">
-					<div class="col-md-6">Galeria</div>		
+					<div class="col-md-6">
+
+
+
+
+
+						<?php $images = get_field('galeria');
+						if( $images ): ?>
+						<div class="gallery">
+						    <ul class="galeria-oficinas">
+						        <?php foreach( $images as $image ): 
+						            $content = '<li>';
+						            $content .= '<a class="gallery_image" href="'. $image['url'] .'">';
+						            $content .= '<img src="'. $image['sizes']['thumbnail'] .'" alt="'. $image['alt'] .'" />';
+						            $content .= '</a>';
+						            $content .= '</li>';
+						        if ( function_exists('slb_activate') ){
+						    	   $content = slb_activate($content);
+						    	}
+						    
+							echo $content;?>
+						        <?php endforeach; ?>
+						    </ul>
+						</div>
+						<?php endif; ?>
+
+
+
+
+
+
+					</div>		
 					<div class="col-md-6">
 						<h2><?php the_title(); ?></h2>
 						<div class="box-conteudo"><?php the_field( 'descricao_das_oficinas' ); ?></div>
